8ddcc3bfa1a7087c796997dedd67a3754c66ed61
Fathalla <fathalla.ebrahem22@gmail.com>
2024-06-13 06:02:02 +0300

    v5.0.1

--- Formula/saml-auth.rb
+++ Formula/saml-auth.rb
@@ -1,9 +1,9 @@
 class SamlAuth < Formula
     desc "SamlAuth for AWS SAML authentication and EKS management"
     homepage "https://github.com/FathAllaTechOps/saml-auth"
-    url "https://github.com/FathAllaTechOps/saml-auth/archive/v5.0.0.tar.gz"
-    sha256 "04340f7fe88966e339f9c692c61fdb38c69d6aa5673dbd222e91c6dc17c33074"
-    version "v5.0.0"
+    url "https://github.com/FathAllaTechOps/saml-auth/archive/v5.0.1.tar.gz"
+    sha256 "268a53393f3a198790e201eab9b645cc99923868cd91ac106a10c04ca4e8057a"
+    version "v5.0.1"
   
     def install
       bin.install "bin/saml.sh" => "saml"
