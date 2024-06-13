3c3261bffd68f5abd412bdb7a65917104d563ab3
Fathalla <fathalla.ebrahem22@gmail.com>
2024-06-13 04:49:52 +0300

    fix sha

--- Formula/saml-auth.rb
+++ Formula/saml-auth.rb
@@ -2,7 +2,7 @@ class SamlAuth < Formula
     desc "SamlAuth for AWS SAML authentication and EKS management"
     homepage "https://github.com/FathAllaTechOps/saml-auth"
     url "https://github.com/FathAllaTechOps/saml-auth/archive/v2.0.0.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "5edc4da3effa2131c24103e23f508a87fb2177cad26722ede09ef9893ed0bbce"
     version "v2.0.0"
   
     def install
