b519dd59e57a4f4edbb985a88320576bac4fb537
fathalla-ebrahem <fathalla.ebrahem@vodafone.com>
2026-05-22 15:35:27 +0300

    bump saml-auth formula to v9.1.0

--- Formula/saml-auth.rb
+++ Formula/saml-auth.rb
@@ -1,9 +1,9 @@
 class SamlAuth < Formula
     desc "SamlAuth for AWS SAML authentication and EKS management"
     homepage "https://github.com/FathAllaTechOps/saml-auth"
-    url "https://github.com/FathAllaTechOps/saml-auth/archive/v7.0.0.tar.gz"
-    sha256 "f4376ddf8dd426a0634378cf79f602318ed340996b6724ea7208c80c7304f3ac"
-    version "v7.0.0"
+    url "https://github.com/FathAllaTechOps/saml-auth/archive/v9.1.0.tar.gz"
+    sha256 "d5f31cba3f6779d5e6afa2cf03a19322be4d756359801b3c12e2234fda84cfb8"
+    version "v9.1.0"
   
     def install
       bin.install "bin/saml.sh" => "saml"
